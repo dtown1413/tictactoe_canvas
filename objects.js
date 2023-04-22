b7183587e40e60762de8873f493be4a2a060fac2
dtown1413 <dustin.vanhoute@gmail.com>
2023-04-22 22:33:48 +0900

    Initial Push

---
+++ objects.js
@@ -0,0 +1,9 @@
+export class DefineLine {
+  constructor(startX, startY, endX, endY, thickness) {
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+    this.thickness = thickness;
+  }
+}
